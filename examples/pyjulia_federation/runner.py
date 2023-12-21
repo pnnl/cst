90414b16b810300b73f2b3780bd2b9a24893187c
Anna Van Boven <anna.van.boven@gmail.com>
2023-12-21 11:00:38 -0700

    add example federation using pyjulia

--- examples/pyjulia_federation/runner.py
+++ examples/pyjulia_federation/runner.py
@@ -6,41 +6,15 @@ from Federate import Federate
 from metadataDB import MetaDB
 import helics as h
 import multiprocessing as mp
-
-# create two federate classes with their own step functions
-class Python_Fed(Federate):
-    
-    def update_internal_model(self):
-        # grab data from other federate
-        val_to_add = self.data_from_federation['inputs']['pyjl_fed/mult_val']
-        if val_to_add == None:
-            val_to_add = 1
-        
-        # perform work on model
-        val_to_add += 1
-        
-        self.data_to_federation['publications']['py_fed/add_val'] = val_to_add
-        
-class PyJulia_Fed(Federate):
-    def __init__(self):
-            importlib = __import__('importlib')
-            self.jl_function = importlib.import_module('julia.Main')
-            self.jl_function.include("jl_functions.jl")
-    
-    def update_internal_model(self):
-        # grab data from other federate
-        val_to_mult = self.data_from_federation['inputs']['py_fed/add_val']
-        if val_to_mult == None:
-            val_to_mult = 1
-        
-        # perform work on model
-        val_to_mult =self.jl_function.multiply_value(val_to_mult)
-        
-        self.data_to_federation['publications']['pyjl_fed/mult_val'] = val_to_mult
+from pyjulia_federates import Python_Fed, PyJulia_Fed
+"""
+This example use case demonstrates how to use julia functions in a Federate using PyJulia. 
+The Federate subclasses can be found in pyjulia_federates, and the julia functions can be found in jl_functions.
+"""
 
 # create a db for a simple federation
 local_default_uri = 'mongodb://localhost:27017'
-metadb = MetaDB(uri_string=local_default_uri)
+metadb = MetaDB(uri=local_default_uri)
 MAX_TIME = 5
 
 # store all dictionaries for the collection
@@ -58,7 +32,6 @@ py_federate_dict = {
       "period": 1,
       "uninterruptible": False,
       "terminate_on_error": True,
-      "wait_for_current_time_update": True,
       "publications": [
         {
           "global": True,
@@ -87,7 +60,6 @@ pyjl_federate_dict = {
       "period": 1,
       "uninterruptible": False,
       "terminate_on_error": True,
-      "wait_for_current_time_update": True,
       "publications": [
         {
           "global": True,
@@ -124,28 +96,45 @@ federation_dict = {
     "jl_fed": pyjl_federate_dict
   }
 
-collection_scenarios = {"current scenario": federation_name}
-# add a DB collection for all tests
-try:
-    metadb.add_dict(collection_name, dict_to_add=collection_scenarios, dict_name="current scenario")
-except NameError as e:
-    metadb.remove_collection(collection_name)
-    metadb.add_collection(collection_name)
-    metadb.add_dict(collection_name, dict_to_add=collection_scenarios, dict_name="current scenario")
-# create and begin a broker for the cosim
-broker = h.helicsCreateBroker("zmq", "mainbroker", f"-f 2")
-#  add a DB collection for the first test federation, and add the federation to the collection
-try:
-    metadb.add_collection(federation_name)
-    metadb.add_dict(federation_name, "federation", federation_dict)
-except NameError as e:
-    metadb.remove_collection(federation_name)
-    metadb.add_collection(federation_name)
-    metadb.add_dict(federation_name, "federation", federation_dict)
-# create the federates
-py_fed = Python_Fed(fed_name = 'py_fed')
-py_fed.connect_to_metadataDB()
-jl_fed = PyJulia_Fed(fed_name = 'jl_fed')
-jl_fed.connect_to_metadataDB()
-py_fed.create_federate()
-jl_fed.create_federate()
+# def federate_cosim(fed:Federate):
+#   fed.connect_to_metadataDB()
+#   fed.create_federate()
+#   fed.run_cosim_loop()
+#   fed.destroy_federate()
+#   return
+
+if __name__ == "__main__":
+  collection_scenarios = {"current scenario": federation_name}
+  # add a DB collection for all tests
+  try:
+      metadb.add_collection(collection_name)
+      metadb.add_dict(collection_name, dict_to_add=collection_scenarios, dict_name="current scenario")
+  except NameError as e:
+      metadb.remove_collection(collection_name)
+      metadb.add_collection(collection_name)
+      metadb.add_dict(collection_name, dict_to_add=collection_scenarios, dict_name="current scenario")
+  # create and begin a broker for the cosim
+  broker = h.helicsCreateBroker("zmq", "mainbroker", f"-f 2")
+  #  add a DB collection for the first test federation, and add the federation to the collection
+  try:
+      metadb.add_collection(federation_name)
+      metadb.add_dict(federation_name, "federation", federation_dict)
+  except NameError as e:
+      metadb.remove_collection(federation_name)
+      metadb.add_collection(federation_name)
+      metadb.add_dict(federation_name, "federation", federation_dict)
+  # create the federates
+  py_fed = Python_Fed(fed_name = 'py_fed')
+  jl_fed = PyJulia_Fed(fed_name = 'jl_fed')
+
+  # connect both federates to the cosim and begin
+  py_process = mp.Process(target=py_fed.run_cosim_loop, args=())
+  jl_process = mp.Process(target=jl_fed.run_cosim_loop, args=())
+  py_process.start()
+  jl_process.start()
+  py_process.join()
+  jl_process.join()
+  # cleanup the cosim
+  metadb.remove_collection(collection_name)
+  metadb.remove_collection(federation_name)
+  h.helicsBrokerDestroy(broker)
