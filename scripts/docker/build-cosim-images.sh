e0ba87b9ae6f94d223d8da53cf384c275061bde2
d3j331 <mitch.pelton@pnnl.gov>
2024-01-24 22:26:18 -0800

    More environment things2

--- scripts/docker/build-cosim-images.sh
+++ scripts/docker/build-cosim-images.sh
@@ -59,8 +59,8 @@ for i in "${!names[@]}"; do
     docker build --no-cache --rm \
                  --build-arg UID=$UID \
                  --build-arg COSIM_USER="${COSIM_USER}" \
-                 --build-arg DOCKER_HOST="$(cat /etc/hostname)" \
-                 --build-arg DOCKER_USER="$(USER)" \
+                 --build-arg DOCKER_HOST="${DOCKER_HOST}" \
+                 --build-arg DOCKER_USER="${DOCKER_USER}" \
                  --network=host \
                  -f "${DOCKERFILE}" \
                  -t "${IMAGE_NAME}" "${CONTEXT}"
