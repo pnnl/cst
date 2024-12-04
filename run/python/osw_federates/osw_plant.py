5abc6ff052d0e845e824373784d7266fb54d516e
Woodstock <tianna-kaye.woodstock@pnnl.gov>
2024-12-04 15:26:49 -0800

    added RT bid to plant

--- run/python/osw_federates/osw_plant.py
+++ run/python/osw_federates/osw_plant.py
@@ -42,10 +42,11 @@ class OSWPLANT(Federate):
         super().__init__(fed_name, **kwargs)
 
         #keys for publications
-        self.rt_bids: list = None # RT Bids
-        self.da_bids: list = None # DA Bids
-        self.res_bids: list = None # RES Bids
-        self.wind_forecasts: list = None # wind forecasts
+        self.rt_bid: float = None # RT Bids
+        self.da_bid: list = None # DA Bids
+        self.res_bid: list = None # RES Bids
+        self.wind_forecast: list = None # wind forecasts
+        self.wind_speed_current_period: float = None
 
         #keys for the subscriptions
         self.rt_dispatch: list = None # RT dispatch
@@ -54,7 +55,12 @@ class OSWPLANT(Federate):
 
         # initialize WF
         self.OneWindFarm = TC.initialize_WindFarm()
+        
+        # load yearlong wind information
         self.fullWS = TC.get_wind_data()
+
+        # Set simulation start time
+        self.sim_start_time = pd.Timestamp(datetime.datetime(2018, 1, 1, 0, 0, 0))
     
     def generate_wind_forecasts(self) -> list:
         """
@@ -68,45 +74,52 @@ class OSWPLANT(Federate):
 
         return wind_forecast
 
-
-    def convert_helics_time(self):
-        ## convert self granted time to datetime
-
-
-        (days, remainder) = divmod(self.granted_time, 86400)
-        (hours, remainder) = divmod(remainder, 3600)
-        (minutes, seconds) = divmod(remainder, 60)
-
-        # m, s = divmod(self.granted_time, 60)
-        # h, m = divmod(m, 60)
-
-        print(self.granted_time, int(minutes), int(hours))
-        Time = pd.Timestamp(datetime.datetime(2018, 1, int(days), int(hours), int(minutes)))
-
-        return Time
-
-
     # 
     def update_internal_model(self):
         """
         """
-        # Time = self.convert_helics_time()
+
+        Time = self.sim_start_time + datetime.timedelta(seconds = self.granted_time)
         # print('Time: ', self.granted_time, Time)
+
+        ## ONLY GENERATE ONE WIND_FORECAST DAILY???? -- adjust to one minute to adjust 
+        if (Time.hour == 0) & (Time.minute == 1) & (Time.second == 0):
+            self.wind_forecast = self.generate_wind_forecasts()
+            print(Time, " ", self.wind_forecast)
+
+
+        # Generate DA Bid at 11:56 AM
+        if (Time.hour == 11) & (Time.minute == 56) & (Time.second == 0):
+            print('Time: ', self.granted_time, Time)
+            # wind_forecast = self.generate_wind_forecasts()
+            self.da_bid = WF.create_day_ahead_energy_bid(self.OneWindFarm, Time, self.wind_forecast)
+            print(Time, " ","DA Bid: ", self.da_bid)
         
-        # # insert market timing check for day ahead and reserves
-        if np.mod(self.granted_time, 60*60*24) == 0: ### TEMP FIT ---- NEED TO FIX IN FUTURE
+        # Generate Reserves at 12:00 PM
+        elif (Time.hour == 12) & (Time.minute == 0) & (Time.second == 0):
+            # print('Time: ', self.granted_time, Time)
+            # wind_forecast = self.generate_wind_forecasts()
+            self.res_bid = WF.create_reserve_bid(self.OneWindFarm, Time, self.wind_forecast)
+            print(Time, " ", "Reserve Bid: ", self.res_bid)
+
+        # Generate RT Bid every 15 minutes --- start after 9:55am and go ad infinitum?
+        if np.mod(Time.minute, 15) == 0:
+            # wind_forecast = self.generate_wind_forecasts()
+            # self.wind_speed_current_period = np.mean(np.mean(self.wind_forecast))
+            self.wind_speed_current_period = random.choice(random.choice(self.wind_forecast))
+            self.rt_bid = WF.create_real_time_energy_bid(self.OneWindFarm, Time, self.wind_speed_current_period)
+            print(Time, "RT Bid: ", self.rt_bid, "WindSpeed: ", self.wind_speed_current_period )
+
+        elif np.mod(Time.minute, 20) == 0:
+            WF.create_dispatch(self.OneWindFarm, Time, [0,self.rt_bid], self.wind_speed_current_period)
+            # print("Dispatch: ", self.wind_speed_current_period )
+            print(Time, ' Dispatched')
 
 
-            wind_forecast = self.generate_wind_forecasts()
-            
-            DTDA = pd.Timestamp(datetime.datetime(2018, 1, 1, 11, 0)) + datetime.timedelta(seconds=self.granted_time)
-            print("Time: ", DTDA )
-            print("WindForecast: ", wind_forecast)
-            print("DA Bid: ", WF.create_day_ahead_energy_bid(self.OneWindFarm, DTDA, wind_forecast))
-            print("Reserve Bid: ", WF.create_reserve_bid(self.OneWindFarm, DTDA, wind_forecast))
 
-            # self.data_to_federation["publication"]["da_bids"] = WF.create_day_ahead_energy_bid(self.OneWindFarm, DA_RE_Time, WindSpeed)
-            # self.data_to_federation["publication"]["res_bids"] = WF.create_reserve_bid(self.OneWindFarm, DA_RE_Time, WindSpeed)
+
+        #     # self.data_to_federation["publication"]["da_bids"] = WF.create_day_ahead_energy_bid(self.OneWindFarm, DA_RE_Time, WindSpeed)
+        #     # self.data_to_federation["publication"]["res_bids"] = WF.create_reserve_bid(self.OneWindFarm, DA_RE_Time, WindSpeed)
 
 
 
