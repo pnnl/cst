73edd5289f7aa05706c8c5135587172b419233ab
matt.cornachione <matt.cornachione@pnnl.gov>
2025-02-14 15:25:03 -0800

    Adding automatic federate kill script to runner.sh

--- run/python/osw_federates/runner.sh
+++ run/python/osw_federates/runner.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source kill_prev.sh
+
 (exec helics_broker -f 1 --loglevel=warning --name=broker &> broker.log &)
 (exec python osw_tso.py OSW_TSO osw_lmp_test_scenario /Users/lill771/Documents/Data/GridView/WECC240_20240807.h5 2032-1-01T00:00:00 2032-01-03T00:00:00 &> OSW_TSO50.log &)
 # (exec python osw_plant.py OSW_Plant osw_lmp_test_scenario &> OSW_PLANT51.log &)
