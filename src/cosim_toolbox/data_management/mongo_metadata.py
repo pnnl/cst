b4ab124fd016b03f443d3de38be31978ffbdd2d3
trevorhardy <trevor.hardy@pnnl.gov>
2025-09-01 12:51:52 -0700

    Add docstrings for mongo_metadata.py

--- src/cosim_toolbox/data_management/mongo_metadata.py
+++ src/cosim_toolbox/data_management/mongo_metadata.py
@@ -89,7 +89,14 @@ class _MongoConnectionHelper:
         self.cst_name_field = "cst_007"
 
     def connect(self) -> bool:
-        """Establishes connection to the MongoDB server."""
+        """Establishes connection to the MongoDB server.
+        
+        Args: 
+            None
+
+        Returns:
+            bool: Flag indicating success of connection to metadata store
+        """
         if self.client:
             return True  # Already connected
         try:
@@ -118,7 +125,14 @@ class _MongoConnectionHelper:
             return False
 
     def disconnect(self) -> None:
-        """Closes the connection to the MongoDB server."""
+        """Closes the connection to the MongoDB server.
+        
+        Args:
+            None
+        
+        Returns:
+            None
+        """
         if self.client:
             self.client.close()
             self.client = None
@@ -126,7 +140,14 @@ class _MongoConnectionHelper:
             logger.debug("MongoDB helper disconnected.")
 
     def get_collection(self, collection_type: str) -> Collection:
-        """Gets a collection object from the database."""
+        """Gets a collection object from the database.
+        
+        Args:
+            collection_type (str): Name of collection type
+
+        Returns:
+            Collection: Collection of the metadata store
+        """
         if self.db is None:
             raise PyMongoError("Not connected to database.")
         if collection_type == "federations":
@@ -166,7 +187,14 @@ class MongoMetadataWriter(MDDataWriter):
             raise ValueError("Must provide either 'helper' or a 'location'.")
 
     def connect(self) -> bool:
-        """Connects to the database, creating the connection if owned."""
+        """Connects to the database, creating the connection if owned.
+        
+        Args:
+            None
+
+        Returns:
+            bool: Flag indicating successful connection to the Mongo database
+        """
         if self._owns_connection:
             if not self.helper.connect():
                 return False
@@ -178,7 +206,14 @@ class MongoMetadataWriter(MDDataWriter):
         return False
 
     def disconnect(self) -> None:
-        """Disconnects from the database if the connection is owned."""
+        """Disconnects from the database if the connection is owned.
+        
+        Args:
+            None
+
+        Returns:
+            None
+        """
         if self._owns_connection:
             self.helper.disconnect()
         self._is_connected = False
@@ -186,11 +221,35 @@ class MongoMetadataWriter(MDDataWriter):
     def write_federation(
         self, name: str, federation_data: Dict[str, Any], overwrite: bool = False
     ) -> bool:
+        """Write federation metadata to MongoDB
+
+        Args:
+            name (str): Name of federation
+            federation_data (Dict[str, Any]): Federation metadata
+            overwrite (bool, optional): Set flag to overwrite any existing 
+                metadata. Defaults to False.
+
+        Returns:
+            bool: Flag indicating data was successfully added to MongoDB 
+                database
+        """
         return self.write("federations", name, federation_data, overwrite)
 
     def write_scenario(
         self, name: str, scenario_data: Dict[str, Any], overwrite: bool = False
     ) -> bool:
+        """Write scenario metadata to MongoDB
+
+        Args:
+            name (str): Name of scenario
+            federation_data (Dict[str, Any]): scenario metadata
+            overwrite (bool, optional): Set flag to overwrite any existing 
+                metadata. Defaults to False.
+
+        Returns:
+            bool: Flag indicating data was successfully added to MongoDB 
+                database
+        """
         return self.write("scenarios", name, scenario_data, overwrite)
 
     def write(
@@ -200,6 +259,22 @@ class MongoMetadataWriter(MDDataWriter):
         data: Dict[str, Any],
         overwrite: bool = False,
     ) -> bool:
+        """Writes arbitrary data to MongoDB database
+
+        Args:
+            collection_type (str): Name of collection type where data is to
+                be written.
+            name (str): Name of metadata to write
+            data (Dict[str, Any]): Metadata to write
+            overwrite (bool, optional): Set to overwrite any exiting metadata
+                of the same name in the MongoDB. Defaults to False.
+
+        Raises:
+            ValidationError: Data to write is not a dictionary
+
+        Returns:
+            bool: Flag indicating data was successfully written to MongoDB.
+        """
         if not self.is_connected:
             logger.error("MongoDB writer not connected. Call connect() first.")
             return False
@@ -280,6 +355,14 @@ class MongoMetadataReader(MDDataReader):
             raise ValueError("Must provide either 'helper' or a 'location'.")
 
     def connect(self) -> bool:
+        """Connect to MongoDB
+
+        Args:
+            None
+
+        Returns:
+            bool: Flag indicating database connection has been established
+        """
         if self._owns_connection:
             if not self.helper.connect():
                 return False
@@ -289,17 +372,50 @@ class MongoMetadataReader(MDDataReader):
         return False
 
     def disconnect(self) -> None:
+        """Disconnects from MongoDB
+
+        Args:
+            None
+        
+        Returns:
+            None
+        """
         if self._owns_connection:
             self.helper.disconnect()
         self._is_connected = False
 
     def read_federation(self, name: str) -> Optional[Dict[str, Any]]:
+        """Reads the specified federation metadata from MongoDB
+
+        Args:
+            name (str): Name of federation to read from MongoDB
+
+        Returns:
+            Optional[Dict[str, Any]]: Federation metadata
+        """
         return self.read("federations", name)
 
     def read_scenario(self, name: str) -> Optional[Dict[str, Any]]:
+        """Reads the specified scenario metadata from MongoDB
+
+        Args:
+            name (str): Name of scenario to read from MongoDB
+
+        Returns:
+            Optional[Dict[str, Any]]: Scenario metadata
+        """
         return self.read("scenarios", name)
 
     def read(self, collection_type: str, name: str) -> Optional[Dict[str, Any]]:
+        """General read from MongoDB
+
+        Args:
+            collection_type (str): Name of collection metadata
+            name (str): Metadata name
+
+        Returns:
+            Optional[Dict[str, Any]]: metadata from MongoDB
+        """
         if not self.is_connected:
             logger.error("MongoDB reader not connected. Call connect() first.")
             return None
@@ -335,12 +451,37 @@ class MongoMetadataReader(MDDataReader):
             return None
 
     def list_federations(self) -> List[str]:
+        """Produces a list of federations stored in the metadata store
+
+        Args:
+            None
+
+        Returns:
+            List[str]: list of federations
+        """
         return self.list_items("federations")
 
     def list_scenarios(self) -> List[str]:
+        """Produces a list of scenarios stored in the metadata store
+
+        Args:
+            None
+
+        Returns:
+            List[str]: list of scenarios
+        """
         return self.list_items("scenarios")
 
     def list_items(self, collection_type: str) -> List[str]:
+        """Produces a list of the named data in the collection in the metadata
+        store.
+
+        Args:
+            collection_type (str): Collection whose contents are being listed
+
+        Returns:
+            List[str]: List of the named data in the specified collection
+        """
         if not self.is_connected:
             logger.error("MongoDB reader not connected.")
             return []
@@ -362,6 +503,14 @@ class MongoMetadataReader(MDDataReader):
             return []
 
     def list_custom_collections(self) -> List[str]:
+        """Lists custom colletion names in metadata store
+
+        Args:
+            None
+
+        Returns:
+            List[str]: List of collection names in metadata store
+        """
         if not self.is_connected or not self.helper.db:
             logger.error("MongoDB reader not connected.")
             return []
@@ -403,6 +552,14 @@ class MongoMetadataManager(MDDataManager):
         self.reader: MongoMetadataReader = MongoMetadataReader(helper=self.helper)
 
     def connect(self) -> bool:
+        """Establish connection for both reader and writer to MongoDB.
+        
+        Args:
+            None
+
+        Returns:
+            bool: Flag indicating that writer and reader are connected
+        """
         if not self.helper.connect():
             return False
 
@@ -413,21 +570,63 @@ class MongoMetadataManager(MDDataManager):
         return True
 
     def disconnect(self) -> None:
+        """Close connection for both reader and writer to MongoDB.
+        
+        Args:
+            None
+
+        Returns:
+            None
+        """
         self.helper.disconnect()
         self.writer.disconnect()
         self.reader.disconnect()
         self._is_connected = False
 
     def delete_federation(self, name: str) -> bool:
+        """Deletes the indicated federation metadata from MongoDB
+
+        Args:
+            name (str): Name of federation to delete
+
+        Returns:
+            bool: Indicates whether the federation metadata has been deleted
+        """
         return self._delete_document("federations", name)
 
     def delete_scenario(self, name: str) -> bool:
+        """Deletes the indicated scenario metadata from MongoDB
+
+        Args:
+            name (str): Name of scenario to delete
+
+        Returns:
+            bool: Indicates whether the scenario metadata has been deleted
+        """
         return self._delete_document("scenarios", name)
 
     def delete(self, collection_type: str, name: str) -> bool:
+        """Deletes the specified metadata
+
+        Args:
+            collection_type (str): Name of the collection type
+            name (str): Name of metadata to delete
+
+        Returns:
+            bool: Flag indicating the data has been deleted.
+        """
         return self._delete_document(collection_type, name)
 
     def _delete_document(self, collection_type: str, name: str) -> bool:
+        """Delete MongoDB document (dictionary) with proper error handling.
+
+        Args:
+            collection_type (str): Name of the collection type
+            name (str): Name of metadata to delete
+
+        Returns:
+            bool: Flag indicating the data has been deleted.
+        """
         if not self._is_connected:
             logger.error("MongoDB manager not connected")
             return False
@@ -458,18 +657,54 @@ class MongoMetadataManager(MDDataManager):
             return False
 
     def exists_federation(self, name: str) -> bool:
+        """Checks to see if federation metadata exists in metadata store
+
+        Args:
+            name (str): Name of federation metadata to check for existance
+
+        Returns:
+            bool: Flag indicating whether federation metadata exists in
+                metadata store
+        """
         return self.exists("federations", name)
 
     def exists_scenario(self, name: str) -> bool:
+        """Checks to see if scenario metadata exists in metadata store
+
+        Args:
+            name (str): Name of scenario metadata to check for existance
+
+        Returns:
+            bool: Flag indicating whether scenario metadata exists in
+                metadata store
+        """
         return self.exists("scenarios", name)
 
     def exists(self, collection_type: str, name: str) -> bool:
+        """Checks to see if metadata exists
+
+        Args:
+            collection_type (str): Collection type name where metadata
+                is being checked for existance.
+            name (str): Metadata name whose existance is being checked.
+
+        Returns:
+            bool: Flag indicating the existance of the named metadata
+        """
         if not self._is_connected:
             return False
         coll = self.helper.get_collection(collection_type)
         return coll.count_documents({self.helper.cst_name_field: name}) > 0
 
     def get_database_stats(self) -> Dict[str, Any]:
+        """Gets MongoDB stats
+
+        Args:
+            None
+
+        Returns:
+            dict: MongoDB database statistics
+        """
         if not self._is_connected or not self.helper.db:
             logger.error("MongoDB manager not connected")
             return {}
