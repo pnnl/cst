72809a363c61ab4d5b1b8ce6e9504df17bdc80a1
d3j331 <mitch.pelton@pnnl.gov>
2025-10-01 01:21:45 -0700

    fix file for CI/CD

--- scripts/docker/build_images.sh
+++ scripts/docker/build_images.sh
@@ -44,7 +44,7 @@ for ((i = 0; i < ${#CONFIG_BUILDS[@]}; i+=3)); do
                  -t "$IMAGE_NAME" "$CONTEXT"
     image2=$(docker images -q "${IMAGE_NAME}")
     if [ "$image1" != "$image2" ]; then
-      print "Deleting old image Id: %s " "$image1"
+      printf "Deleting old image Id: %s " "$image1"
       docker rmi "${image1}"
     fi
   fi
